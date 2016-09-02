7e4d8bc7d251c1d5d530d64b17703343cf545791
miyohide <miyohide@gmail.com>
2016-09-02 23:45:36 +0900

    add gps_example for CRuby

---
+++ gps_example/gps_example.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'serialport'
+
+SerialPort.open('/dev/ttyUSB0', 4800, 8, 1, SerialPort::NONE) { |sp|
+  loop do
+    begin
+      line = sp.readline
+      if line =~ /^\$GPGGA/
+        columns = line.split(",")
+        puts "time = #{columns[1]}, keido = #{columns[2]}, ido = #{columns[4]}"
+        break
+      end
+    rescue ArgumentError
+      # 連続して起動するとArgumentErrorが起きるのでとりあえず再処理させる
+    end
+  end
+}
+
